6127528181ef02b5ca673e58997f765dff33c065
asadakber <asad.akberweb@gmail.com>
2020-03-29 12:43:00 +0500

    first commit

---
+++ imports/ui/Task.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import classnames from 'classnames';
+import { Tasks } from '../api/task';
+ 
+
+export default class Task extends Component {
+  toggleChecked() {
+    Meteor.call('tasks.setChecked', this.props.task._id, !this.props.task.checked);
+  }
+ 
+  deleteThisTask() {
+    Meteor.call('tasks.remove', this.props.task._id);
+  }
+
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+  }
+ 
+  render() {
+    // const taskClassName = this.props.task.checked ? 'checked' : '';
+
+    const taskClassName = classnames({
+      checked: this.props.task.checked,
+      private: this.props.task.private,
+    });
+ 
+    return (
+      <li classnames={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+ 
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+        { this.props.showPrivateButton ? (
+          <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+            { this.props.task.private ? 'Private' : 'Public' }
+          </button>
+        ) : ''}
+        <span className="text">
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
+      </li>
+    );
+  }
+}
